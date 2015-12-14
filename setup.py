464e02402dfdfd86da30a355ee1d70bde9638f0d
milimetric <dandreescu@wikimedia.org>
2015-12-14 09:28:15 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def requirements(fname):
 
 setup(
     name='mwviews',
-    version='0.0.1',
+    version='0.0.2',
     author='Dan Andreescu',
     author_email='dandreescu@wikimedia.org',
     description='Tools for parsing and querying Wikimedia Foundation' +
