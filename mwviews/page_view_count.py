0bb0d100b25254fc51c81913a5c23dd016e9220b
hall1467 <hall1467@umn.edu>
2017-05-26 16:08:15 -0500

    Fixed issue that occurs when spaces exist in page titles

--- mwviews/page_view_count.py
+++ mwviews/page_view_count.py
@@ -12,7 +12,18 @@ class PageViewCount:
 
     @classmethod
     def from_line(cls, line):
-        project, page_name, views, bytes_returned = line.strip().split(" ")
+        line_split_on_spaces = line.strip().split(" ")
+
+        views = line_split_on_spaces[-2]
+        del(line_split_on_spaces[-2])
+
+        bytes_returned = line_split_on_spaces[-1]
+        del(line_split_on_spaces[-1])
+
+        project = line_split_on_spaces[0]
+        del(line_split_on_spaces[0])
+        
+        page_name = " ".join(line_split_on_spaces)
         page_name = urllib.parse.unquote(page_name).split("#")[0]  # No anchors
 
         return cls(project, page_name, int(views), int(bytes_returned))
