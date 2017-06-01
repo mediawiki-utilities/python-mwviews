b9ccdce949b711dd464913614938d3096b3e215d
hall1467 <hall1467@umn.edu>
2017-06-01 15:09:46 -0500

    Updated, partially based on code review

--- mwviews/utilities/fetch_global_namespaces.py
+++ mwviews/utilities/fetch_global_namespaces.py
@@ -1,14 +1,13 @@
 """
-Fetches a JSON file containing information about all namespace
-names and aliases across all wikis using
-action=query&meta=siteinfo.  This file is used later
-to parse page names into (namespace, title)
-pairs.
+Fetches a JSON file containing information about all namespace names and aliases
+across all wikis using action=query&meta=siteinfo.  This file is used later to 
+parse page names into (namespace, title) pairs.
 
 Usage:
     fetch_global_namespaces (-h|--help)
     fetch_global_namespaces [<dbname-file>]
                             [--api-agent=<username>]
+                            [--output=<path>]
                             [--debug] 
                             [--verbose]
 
@@ -16,7 +15,11 @@ Options:
     -h, --help             Prints this documentation
     <dbname-file>          Path to json file to process. If no file is 
                            provided, uses stdin
-   --api-agent=<username>  Username for API session. [default: Unknown]
+   --api-agent=<username>  Username for API session. Please provide name and
+                           email, in quotes
+                           (e.g. "John Doe johndoe@johndoe.com"). If api agent 
+                           not specified, MWAPI warning will be printed.
+   --output=<path>         A file to write output to. [default: <stdout>]
     --debug                Print debug logging to stderr
     --verbose              Print dots and stuff to stderr  
 """
@@ -37,67 +40,76 @@ def main(argv=None):
     )
 
     if args['<dbname-file>']:
-        dbname_file = args['<dbname-file>']
+        dbname_file = open(args['<dbname-file>'], "r")
     else:
         logger.info("Reading from <stdin>")
         dbname_file = sys.stdin
 
     api_agent = args['--api-agent']
+
+    if args['--output'] == '<stdout>':
+        output = sys.stdout
+    else:
+        output = open(args['--output'], "w")
+
     verbose = args['--verbose']
 
-    run(dbname_file, api_agent, verbose)
+    run(dbname_file, api_agent, output, verbose)
 
-def run(dbname_file, api_agent, verbose):
-
+def run(dbname_file, api_agent, output, verbose):
     canonical_namespaces = defaultdict(lambda : defaultdict(dict))
-    temp_namespace_id_to_information = defaultdict(lambda : defaultdict(dict))
+    namespace_id_to_entry = defaultdict(lambda : defaultdict(dict))
 
     for line in dbname_file:
 
-        wikidb_dictionary = json.loads(line)
+        wikidb_dict = json.loads(line)
 
         if verbose:
-            sys.stderr.write("Processing: " + str(wikidb_dictionary) + "\n")
+            sys.stderr.write("Processing: " + str(wikidb_dict) + "\n")
             sys.stderr.flush()
 
+        if api_agent:
+            user_agent_string = "user_agent=" + api_agent
+        else:
+            user_agent_string = None
+
         try:
-            namespace_api_information =\
-            mwapi.Session(wikidb_dictionary['wikiurl'], user_agent=api_agent).\
+            api_namespaces =\
+            mwapi.Session(wikidb_dict['wikiurl'],user_agent_string).\
             get(action="query", meta="siteinfo", 
             siprop="namespaces|namespacealiases")
         except mwapi.errors.APIError as e:
             logger.warn("{0}. Skipping {1}".
-                format(e, wikidb_dictionary['dbname']))
+                format(e, wikidb_dict['dbname']))
             continue
         except mwapi.errors.ConnectionError as e:
             logger.warn("{0}. Skipping {1}".
-                format(e, wikidb_dictionary['dbname']))
+                format(e, wikidb_dict['dbname']))
             continue
 
 
-        namespace_api_entries = namespace_api_information['query']['namespaces']
-        for namespace_id in namespace_api_entries:
-            current_information = namespace_api_entries[namespace_id]
-            namespace = current_information['*']
-            if 'canonical' not in current_information:
-                current_information['canonical'] = namespace
-            del(current_information['*'])
-            canonical_namespaces[wikidb_dictionary['dbname']][namespace] =\
-                current_information
-            temp_namespace_id_to_information\
-                [wikidb_dictionary['dbname']]\
-                [current_information['id']] = current_information
+        api_namespace_entries = api_namespaces['query']['namespaces']
+        
+        for namespace_id in api_namespace_entries:
+            current_entry = api_namespace_entries[namespace_id]
+            namespace = current_entry['*']
 
-        namespace_alias_api_entries =\
-            namespace_api_information['query']['namespacealiases']
+            if 'canonical' not in current_entry:
+                current_entry['canonical'] = namespace
 
-        for namespace_aliases in namespace_alias_api_entries:
-            canonical_namespaces[wikidb_dictionary\
-                ['dbname']]\
-                [namespace_aliases['*']] = temp_namespace_id_to_information\
-                                               [wikidb_dictionary['dbname']]\
-                                               [namespace_aliases['id']]
+            del(current_entry['*'])
+
+            canonical_namespaces[wikidb_dict['dbname']][namespace] =\
+                current_entry
+            namespace_id_to_entry[wikidb_dict['dbname']][current_entry['id']] =\
+                current_entry
+
+        alias_api_entries = api_namespaces['query']['namespacealiases']
+
+        for aliases in alias_api_entries:
+            canonical_namespaces[wikidb_dict['dbname']][aliases['*']] =\
+                namespace_id_to_entry[wikidb_dict['dbname']][aliases['id']]
 
 
-    sys.stdout.write(json.dumps(canonical_namespaces))
+    output.write(json.dumps(canonical_namespaces))
 
